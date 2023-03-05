1fa0b22e2a92e2c87000e6644b3116ad65be0924
Mateusz <mateusz.politycki@gmail.com>
2023-03-05 23:03:51 +0100

    feat: trigger greedy cycle algorithm in main, add requirements.txt

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ import utils.io as io
 import lab1.nearest_insertion as nnwi
 import lab1.greedy_cycle as gc
 import lab1.scoring as scoring
+from utils.visualization import visualize_graph
 
 N_INSTANCES = 100
 DATA_DIR = 'data'
+RESULT_DIR = io.directory('result')
 FILES = ['kroa100.tsp', 'krob100.tsp']
 ALGORITHMS = {"Nearest insertion": nnwi.tcp_nearest_insertion, "Greedy Cycle": gc.tcp_greedy_cycle}
 
 if __name__ == "__main__":
-    distance_graphs = [io.create_graph(io.load_data(os.path.join(DATA_DIR, f), 6)) for f in FILES]
+    loaded_files = {f: io.load_data(os.path.join(DATA_DIR, f), 6) for f in FILES}
+    distance_graphs = {file_name: io.create_graph(loaded_file) for file_name, loaded_file in loaded_files.items()}
 
-    results : Dict[str, pd.DataFrame] = {}  # {algorithm_name: pd.DataFrame(alogorithm_results))}
-    best_solutions : Dict[str, Dict[str, List[int]]] = {} # {algorithm_name: {file_name: best_solution}}
+    results: Dict[str, pd.DataFrame] = {}  # {algorithm_name: pd.DataFrame(alogorithm_results))}
+    best_solutions: Dict[str, Dict[str, List[int]]] = {} # {algorithm_name: {file_name: best_solution}}
 
     # Run each algorithm
     for name, algorithm in ALGORITHMS.items():
@@ -27,7 +30,7 @@ if __name__ == "__main__":
         best_algorithm_solutions = {}
 
         # Run the algorithm for each file
-        for file_name, distance_graph in zip(FILES, distance_graphs):
+        for file_name, distance_graph in distance_graphs.items():
             file_names += [file_name] * N_INSTANCES
             min_cycle_length = np.inf
             best_solution = None
@@ -48,6 +51,13 @@ if __name__ == "__main__":
         results[name] = pd.DataFrame({'file': file_names, 'cycles_length': algorithm_results})
         best_solutions[name] = best_algorithm_solutions
 
-    print(results['Nearest insertion'])
-    print(results['Greedy Cycle'])
+
+    visualize_graph(best_solutions['Greedy Cycle']['kroa100.tsp'], loaded_files['kroa100.tsp'],
+                    f'{RESULT_DIR}/greedy_cycle_kroa100.png')
+    visualize_graph(best_solutions['Greedy Cycle']['krob100.tsp'], loaded_files['krob100.tsp'],
+                    f'{RESULT_DIR}/greedy_cycle_krob100.png')
+    visualize_graph(best_solutions['Nearest insertion']['kroa100.tsp'], loaded_files['kroa100.tsp'],
+                    f'{RESULT_DIR}/nearest_insertion_kroa100.png')
+    visualize_graph(best_solutions['Nearest insertion']['krob100.tsp'], loaded_files['krob100.tsp'],
+                    f'{RESULT_DIR}/nearest_insertion_krob100.png')
             
