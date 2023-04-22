08979eef03be09d89e02edd0e93b23f65b3e0373
Mateusz <mateusz.politycki@gmail.com>
2023-04-22 13:56:32 +0200

    feat(lab3): add candidates moves algorithm, add kroa200 and krob200, prepare script

---
+++ lab3/algorithms/candidate_moves.py
@@ -0,0 +1,63 @@
+import numpy as np
+from lab2.action_generators import Action
+import lab2.action_generators.out_cycle_vertices_swap_generator as out_cycle_vertices
+import lab2.action_generators.edges_swap_generator as edges_swap
+
+
+class CandidateMoves:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_nearest_neighbours(distance_matrix):
+        nearest_neighbours = []
+        for i in range(len(distance_matrix)):
+            nearest_neighbours.append(np.argsort(distance_matrix[i])[1:11])
+        return nearest_neighbours
+
+    @staticmethod
+    def get_node_info(node, solution):
+        cycle = 0 if node in solution[0] else 1
+        return cycle
+
+    def __call__(self, distance_matrix, solution):
+        nearest_neighbours = self.get_nearest_neighbours(distance_matrix)
+
+        while True:
+            best_action = None
+            for node in range(len(nearest_neighbours)):
+                for neighbour in nearest_neighbours[node]:
+                    node_cycle = self.get_node_info(node, solution)
+                    neighbour_cycle = self.get_node_info(neighbour, solution)
+
+                    if node_cycle == neighbour_cycle:
+                        action = Action(
+                            name="swapEdges",
+                            i=node,
+                            j=neighbour,
+                            cycle_index=node_cycle,
+                            delta=edges_swap.calculate_delta(solution[node_cycle], node, neighbour, distance_matrix),
+                            do=edges_swap.swap_edges_inside_cycle,
+                        )
+                    else:
+                        if node_cycle != 0:
+                            node, neighbour = neighbour, node
+                        action = Action(
+                            name="swapVerticesOutsideCycle",
+                            i=node,
+                            j=neighbour,
+                            cycle_index=-1,
+                            delta=out_cycle_vertices.calculate_delta(solution, node, neighbour, distance_matrix),
+                            do=out_cycle_vertices.swap_vertices_outside_cycle,
+                        )
+                    if best_action is None or action.delta < best_action.delta:
+                        best_action = action
+
+            if best_action is not None and best_action.delta < 0:
+                if best_action.name == "swapEdges":
+                    best_action.do(solution[best_action.cycle_index], best_action.i, best_action.j)
+                else:
+                    best_action.do(solution, best_action.i, best_action.j)
+            else:
+                break
+        return solution
