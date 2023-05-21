253d6324c28164d3a5f273aa352b5248785b8787
Wojciech Spychalski <wspychalski00@gmail.com>
2023-05-21 17:51:09 +0200

    feat/fix(lab5): Add script and fix bugs in evolutionary algorithm

--- lab5/algorithms/evolutionary_algorithm.py
+++ lab5/algorithms/evolutionary_algorithm.py
@@ -3,15 +3,20 @@ import time
 import random
 import numpy as np
 
-from utils.scoring import get_cycle_length
+from utils.scoring import get_summary_score
 
+from lab1.greedy_cycle_two_regret_heuristic import tcp_greedy_cycle_two_regret_heuristic
+from lab2.solution_initializers import RandomSolutionGenerator
+from lab3.algorithms.SteepestSearchWithMemory import SteepestSearchWithMemory
 
 
 
 class EvolutionaryAlgorithm:
-    def __init__(self, population_size, max_time, minimal_difference, 
-                 vertex_mutation_chance, patience, ls_on_new_solution,
-                 solution_initializer, heuristic_algorithm, ls_algorithm):
+    def __init__(self, max_time, population_size=20, minimal_difference=40, 
+                 vertex_mutation_chance=0.2, patience=300, ls_on_new_solution=False,
+                 solution_initializer=RandomSolutionGenerator,
+                 heuristic_algorithm=tcp_greedy_cycle_two_regret_heuristic,
+                 ls_algorithm=SteepestSearchWithMemory):
         self.population_size = population_size
         self.max_time = max_time
         self.minimal_difference = minimal_difference
@@ -23,7 +28,6 @@ class EvolutionaryAlgorithm:
         self.ls_algorithm = ls_algorithm
 
     def _recombine(self, parent1_solution, parent2_solution, distance_matrix):
-        remaining = []
         new_solution = deepcopy(parent1_solution)
 
         # check for the same edges in both parent solutions
@@ -40,7 +44,6 @@ class EvolutionaryAlgorithm:
                             in_both_cycles = True
                             break
                 if not in_both_cycles:
-                    remaining.extend([v1, v2])
                     new_solution[i_cycle][i] = new_solution[i_cycle][(i + 1) % len(cycle1)] = -1
         
         # check for vertices without edges
@@ -48,14 +51,12 @@ class EvolutionaryAlgorithm:
             for i in range(1, len(cycle)):
                 prev, curr, next = cycle[i - 1], cycle[i], cycle[(i + 1) % len(cycle)]
                 if prev == next == -1 and curr != -1:
-                    remaining.append(curr)
                     new_solution[i_cycle][i] = -1
 
         # mutations
         for i_cycle, cycle in enumerate(new_solution):
             for i in range(len(cycle)):
                 if cycle[i] != -1 and random.random() < self.vertex_mutation_chance:
-                    remaining.append(object)
                     new_solution[i_cycle][i] = -1
         
         # delete marked vertices
@@ -63,7 +64,6 @@ class EvolutionaryAlgorithm:
             new_solution[i_cycle] = [v for v in cycle if v != -1]
 
         # rebuild cycles with heuristic algorithm
-        # TODO: check, remaining is probably not needed here
         new_solution = self.heuristic_algorithm(distance_matrix, new_solution)
 
         return new_solution
@@ -75,16 +75,16 @@ class EvolutionaryAlgorithm:
         population = []
         for _ in range(self.population_size):
             random_solution = self.solution_initializer(distance_matrix)
-            ls_solution = self.ls_algorithm(distance_matrix, random_solution)
-            score = get_cycle_length(distance_matrix, ls_solution)
+            ls_solution = self.ls_algorithm(random_solution)(distance_matrix)
+            score = get_summary_score(distance_matrix, ls_solution)
             population.append((score, ls_solution))
 
         iterations_without_improvement = 0
-        best_score, best_solution = *min(population, key=lambda x: x[0])
+        best_score, best_solution = min(population, key=lambda x: x[0])
         worst_unit_index = np.argmax([x[0] for x in population])
-        worst_score, worst_solution = *population[worst_unit_index]
+        worst_score, worst_solution = population[worst_unit_index]
 
-        while tiem.time() - start < self.max_time:
+        while time.time() - start < self.max_time:
             # select parents
             parent1, parent2 = random.sample(population, 2)
             parent1_solution, parent2_solution = parent1[1], parent2[1]
@@ -92,8 +92,8 @@ class EvolutionaryAlgorithm:
             # recombine
             new_solution = self._recombine(parent1_solution, parent2_solution, distance_matrix)
             if self.ls_on_new_solution:
-                new_solution = self.ls_algorithm(distance_matrix, new_solution)
-            new_solution_score = get_cycle_length(distance_matrix, new_solution)
+                new_solution = self.ls_algorithm(new_solution)(distance_matrix)
+            new_solution_score = get_summary_score(distance_matrix, new_solution)
 
             too_similar = any(abs(new_solution_score - score) < self.minimal_difference for score, _ in population)
 
@@ -102,12 +102,12 @@ class EvolutionaryAlgorithm:
                 best_score, best_solution = new_solution_score, new_solution
                 population[worst_unit_index] = (new_solution_score, new_solution)
                 worst_unit_index = np.argmax([x[0] for x in population])
-                worst_score, worst_solution = *population[worst_unit_index]
+                worst_score, worst_solution = population[worst_unit_index]
                 iterations_without_improvement = 0
             elif not too_similar and new_solution_score < worst_score:
                 population[worst_unit_index] = (new_solution_score, new_solution)
                 worst_unit_index = np.argmax([x[0] for x in population])
-                worst_score, worst_solution = *population[worst_unit_index]
+                worst_score, worst_solution = population[worst_unit_index]
                 iterations_without_improvement += 1
             else:
                 iterations_without_improvement += 1
--- utils/scoring.py
+++ utils/scoring.py
@@ -14,3 +14,17 @@ def get_cycle_length(distance_graph: np.ndarray, cycle: List[int]) -> int:
         cycle_length += distance_graph[cycle[i]][cycle[i + 1]]
     cycle_length += distance_graph[cycle[-1]][cycle[0]]
     return cycle_length
+
+def get_summary_score(distance_graph: np.ndarray, cycles: List[List[int]]) -> int:
+    """
+    Get summary score of cycles
+    :param cycles: list of cycles
+    :return: summary score
+    """
+    summary_score = 0
+    for cycle in cycles:
+        summary_score += get_cycle_length(distance_graph, cycle)
+    return summary_score
+
+
+
